0ec0be3f6576c1cbe764e63dcd12ad16c050b828
unullmass <codemanure@live.co.uk>
2018-05-16 01:30:59 +0530

    Added code to unescape HTML entities in Q&A data.

--- app/src/main/java/quizza/unullmass/net/quizza/MainActivity.java
+++ app/src/main/java/quizza/unullmass/net/quizza/MainActivity.java
@@ -1,7 +1,10 @@
 package quizza.unullmass.net.quizza;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -19,16 +22,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.TreeMap;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
     private final int MAX_QUESTIONS = 10;
     private final int MAX_CHOICES = 4;
     private boolean quizRunning = false;
-    private ArrayList<HashMap<String, String>> qa = new ArrayList<HashMap<String, String>>();
+    private ArrayList<HashMap<String, String>> qa = new ArrayList<>();
     private int rightAnswers = 0;
     private int quesRemaining = MAX_QUESTIONS;
     private boolean[] askedQ = new boolean[MAX_QUESTIONS];
@@ -47,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
      * @param ansRadio - reference to the radiobutton widget that was chosen as the answer
      */
     public void checkAnswer(View ansRadio) {
-        quesRemaining--;
 
         // which radio button was clicked - get the answer chosen
         String anschoice = ((RadioButton) ansRadio).getText().toString();
@@ -64,7 +71,10 @@ public class MainActivity extends AppCompatActivity {
         // load remaining questions
         if (quesRemaining > 0) {
             loadNextQuestion();
+        } else {
+            endQuiz();
         }
+
     }
 
     /**
@@ -73,14 +83,16 @@ public class MainActivity extends AppCompatActivity {
      * Loads the first question
      */
     public void startQuiz() {
-        getQandA();
-        loadNextQuestion();
+        if (getQandA()) {
+            quizRunning = true;
+            for (boolean q : askedQ) {
+                q = false;
+            }
 
-        for (boolean q : askedQ) {
-            q = false;
+            loadNextQuestion();
+        } else {
+            Toast.makeText(this, "Failed to fetch questions.", Toast.LENGTH_SHORT).show();
         }
-
-
     }
 
     /**
@@ -89,27 +101,42 @@ public class MainActivity extends AppCompatActivity {
      * @param view - this is the view reference of the Reset button that transmits the click event
      */
     public void resetQuiz(View view) {
-        Button resetBtn = (Button) findViewById(R.id.btn_resetnew);
+        doResetQuiz();
+    }
+
+    public void doResetQuiz() {
+        Button resetBtn = findViewById(R.id.btn_resetnew);
 
         // check if the quiz is running
-        if (quizRunning) {
+        if (!quizRunning) {
             if (resetBtn.getText().equals(getString(R.string.new_quiz))) {
                 startQuiz();
+                quizRunning = true;
+                resetBtn.setText(getString(R.string.reset_quiz));
             }
         } else {
-            ArrayList<HashMap<String, String>> qa = new ArrayList<HashMap<String, String>>();
+            ArrayList<HashMap<String, String>> qa = new ArrayList<>();
             rightAnswers = 0;
             quesRemaining = MAX_QUESTIONS;
+            qa = new ArrayList<>();
+            askedQ = new boolean[MAX_QUESTIONS];
+            correctans = "";
+            quizRunning = false;
+
             // reset the text views to their default state
             updateMetrics();
-            ((TextView) findViewById(R.id.textview_question)).setText(getString(R.string.blank_text));
+            ((TextView) findViewById(R.id.textview_question)).setText("");
 
             // clear the radio groups
-            RadioGroup ans_rg = ((RadioGroup) findViewById(R.id.rg_answerchoices));
+            RadioGroup ans_rg = findViewById(R.id.rg_answerchoices);
             ans_rg.clearCheck();
             for (int i = 0; i < ans_rg.getChildCount(); i++) {
-                ((RadioButton) ans_rg.getChildAt(i)).setText(getString(R.string.blank_text));
+                RadioButton rb = ((RadioButton) ans_rg.getChildAt(i));
+                rb.setText("");
+                rb.setEnabled(false);
             }
+            resetBtn.setText(getString(R.string.new_quiz));
+
         }
 
     }
@@ -124,112 +151,170 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method ends the quiz and gives the user their final score
+     */
+    public void endQuiz() {
+        // display a toast with the final score
+        if (rightAnswers == 10) {
+            Toast.makeText(this, "You answered ALL questions correctly! A TOAST to you!", Toast.LENGTH_LONG).show();
+        } else if (rightAnswers > 1) {
+            Toast.makeText(this, "You answered " + rightAnswers + " questions correctly! Keep going!", Toast.LENGTH_LONG).show();
+        } else if (rightAnswers == 1) {
+            Toast.makeText(this, "You answered 1 question correctly! Try harder!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "You answered no questions correctly! Don't give up!", Toast.LENGTH_LONG).show();
+        }
+
+        // Disable the choices so no more questions can be answered
+        doResetQuiz();
+    }
+
+    /**
      * This method loads the next question onto the screen
      */
     public void loadNextQuestion() {
-        Random r = new java.util.Random();
+        if (quesRemaining == 0) {
+            endQuiz();
+        } else {
+            Random r = new java.util.Random();
 
-        // pick a question among the ones that haven't been asked
-        int nextqindex = r.nextInt(MAX_QUESTIONS);
-        while (askedQ[nextqindex]) {
-            nextqindex = r.nextInt(MAX_QUESTIONS);
-        }
+            // pick a question among the ones that haven't been asked
+            int nextqindex = r.nextInt(MAX_QUESTIONS);
+            while (askedQ[nextqindex]) {
+                nextqindex = r.nextInt(MAX_QUESTIONS);
+            }
 
-        // load question
-        HashMap<String, String> nextq = qa.get(nextqindex);
+            // load question
+            HashMap<String, String> nextq = qa.get(nextqindex);
 
-        String question = nextq.get("question");
-        correctans = nextq.get("correct_answer");
+            // set question as asked
+            askedQ[nextqindex] = true;
 
-        TreeMap<Integer, String> choices = new TreeMap<Integer, String>();
+            String question = nextq.get("question");
+            correctans = nextq.get("correct_answer");
 
-        boolean[] setC = new boolean[MAX_CHOICES];
-        for (boolean c : setC) {
-            c = false;
-        }
+            TreeMap<Integer, String> choices = new TreeMap<>();
 
-        // we need to randomly sort the options
-        int nextcindex = r.nextInt(MAX_CHOICES);
-        int qset = 0;
-        while (qset < MAX_CHOICES) {
-            if (!setC[nextcindex]) {
-                choices.put(nextcindex, nextq.get("choice" + nextcindex));
-                qset++;
+            boolean[] setC = new boolean[MAX_CHOICES];
+            for (boolean c : setC) {
+                c = false;
+            }
+
+            // we need to randomly sort the options
+            int nextcindex = r.nextInt(MAX_CHOICES);
+            int qset = 0;
+            while (qset < MAX_CHOICES) {
+                if (!setC[nextcindex]) {
+                    choices.put(nextcindex, nextq.get("choice" + nextcindex));
+                    setC[nextcindex] = true;
+                    qset++;
+                }
+                nextcindex = r.nextInt(MAX_CHOICES);
+            }
+
+            // update questions remaining
+            quesRemaining--;
+            updateMetrics();
+
+            // set the question text in the UI
+            ((TextView) findViewById(R.id.textview_question)).setText(question);
+
+            // set the choices in the radio group
+            RadioGroup ans_rg = findViewById(R.id.rg_answerchoices);
+            ans_rg.clearCheck();
+            for (int i = 0; i < ans_rg.getChildCount(); i++) {
+                RadioButton rb = ((RadioButton) ans_rg.getChildAt(i));
+                rb.setText(choices.get(i));
+                rb.setEnabled(true);
+
             }
         }
-
-
-        updateMetrics();
-
-        // set the question text in the UI
-        ((TextView) findViewById(R.id.textview_question)).setText(question);
-
-        // set the choices in the radio group
-        RadioGroup ans_rg = ((RadioGroup) findViewById(R.id.rg_answerchoices));
-        for (int i = 0; i < ans_rg.getChildCount(); i++) {
-            ((RadioButton) ans_rg.getChildAt(i)).setText(choices.get(i));
-        }
-
-
-
-
     }
 
-    private void getQandA() {
-        URL url;
-        HttpURLConnection urlConnection = null;
-        try {
-            url = new URL(getString(R.string.get_ques_url));
+    private boolean getQandA() {
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
 
-            urlConnection = (HttpURLConnection) url
-                    .openConnection();
+            URL url;
+            HttpsURLConnection urlConnection = null;
+            try {
+                url = new URL(URLDecoder.decode(getString(R.string.get_ques_url)));
 
-            InputStream in = urlConnection.getInputStream();
 
-            InputStreamReader isw = new InputStreamReader(in);
-            BufferedReader bf = new BufferedReader(isw);
+                if (url.getProtocol().toLowerCase().equals("https")) {
+                    urlConnection = (HttpsURLConnection) url
+                            .openConnection();
+                }
 
-            StringBuilder data = new StringBuilder();
-            String line = "";
+                InputStream in;
+                if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    in = urlConnection.getInputStream();
+                    InputStreamReader isw = new InputStreamReader(in);
+                    BufferedReader bf = new BufferedReader(isw);
 
-            while ((line = bf.readLine()) != null) {
-                data.append(line);
-            }
+                    StringBuilder data = new StringBuilder();
+                    String line;
 
-            JSONObject jo = new JSONObject(data.toString());
-
-            JSONArray jArray = jo.getJSONArray("results");
-
-            for (int i = 0; i < jArray.length(); i++) {
-                try {
-                    JSONObject oneObject = jArray.getJSONObject(i);
-                    // Pulling items from the array
-                    String question = oneObject.getString("question");
-                    String rightans = oneObject.getString("correct_answer");
-                    // parse the array containing wrong answers
-                    JSONArray jArray_wrongans = oneObject.getJSONArray("incorrect_answers");
-
-                    qa.add(new HashMap<String, String>());
-                    HashMap<String, String> h = qa.get(i);
-                    h.put("question", question);
-                    h.put("correct_answer", rightans);
-
-                    for (int j = 0; j < jArray_wrongans.length(); j++) {
-                        h.put("choice" + (j + 1), jArray_wrongans.getString(j));
+                    while ((line = bf.readLine()) != null) {
+                        data.append(line);
                     }
-                    h.put("choice0", rightans);
 
-                } catch (JSONException e) {
+                    JSONObject jo = new JSONObject(data.toString());
 
+                    JSONArray jArray = jo.getJSONArray("results");
+
+                    for (int i = 0; i < jArray.length(); i++) {
+                        try {
+                            JSONObject oneObject = jArray.getJSONObject(i);
+                            // Pulling items from the array
+                            String question = Html.fromHtml(oneObject.getString("question")).toString();
+                            String rightans = Html.fromHtml(oneObject.getString("correct_answer")).toString();
+                            // parse the array containing wrong answers
+                            JSONArray jArray_wrongans = oneObject.getJSONArray("incorrect_answers");
+
+                            qa.add(new HashMap<String, String>());
+                            HashMap<String, String> h = qa.get(i);
+                            h.put("question", question);
+                            h.put("correct_answer", rightans);
+
+                            for (int j = 0; j < jArray_wrongans.length(); j++) {
+                                h.put("choice" + (j + 1), Html.fromHtml(jArray_wrongans.getString(j)).toString());
+                            }
+                            h.put("choice0", rightans);
+
+                        } catch (JSONException e) {
+                            Log.e(this.getLocalClassName(), "Error parsing JSON!");
+                            return false;
+                        }
+                    }
+
+                } else {
+                    in = urlConnection.getErrorStream();
+                    InputStreamReader isw = new InputStreamReader(in);
+                    BufferedReader bf = new BufferedReader(isw);
+
+                    StringBuilder data = new StringBuilder();
+                    String line;
+
+                    while ((line = bf.readLine()) != null) {
+                        data.append(line);
+                    }
+                    Log.e(this.getLocalClassName(), data.toString());
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
                 }
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
         }
+        return true;
     }
 }
